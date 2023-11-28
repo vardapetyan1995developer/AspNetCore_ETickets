598191ee775583c5bd308959858850efecd0e20b
Narek <vardapetyannarek0@gmail.com>
2023-11-28 16:20:06 +0400

    EntityBaseRepository files

---
+++ ETickets2023/Data/Base/IEntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace ETickets2023.Data.Base
+{
+    public interface IEntityBase
+    {
+        int Id { get; set; }
+    }
+}
---
+++ ETickets2023/Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using ETickets2023.Models;
+
+namespace ETickets2023.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task<T> UpdateAsync(int id, T entity);
+        Task DeleteAsync(int id);
+    }
+}
