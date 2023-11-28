ad9172f216d843b75b840ef0ea0c1f52ca9dbe9f
Narek <vardapetyannarek0@gmail.com>
2023-11-28 21:58:05 +0400

    GetAllAsync() & GetByIdAsync() base methods

--- ETickets2023/Data/Base/EntityBaseRepository.cs
+++ ETickets2023/Data/Base/EntityBaseRepository.cs
@@ -1,8 +1,18 @@
 ﻿
+using ETickets2023.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace ETickets2023.Data.Base
 {
     public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
+        private readonly AppDbContext _context;
+
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public Task AddAsync(T entity)
         {
             throw new NotImplementedException();
@@ -13,15 +23,20 @@ namespace ETickets2023.Data.Base
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<T>> GetAllAsync()
+        public async Task<IEnumerable<T>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var result = await _context.Set<T>().ToListAsync();
+
+            return result;
         }
 
-        public Task<T> GetByIdAsync(int id)
+        public async Task<T> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var result = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+
+            return result;
         }
+
 
         public Task<T> UpdateAsync(int id, T entity)
         {
